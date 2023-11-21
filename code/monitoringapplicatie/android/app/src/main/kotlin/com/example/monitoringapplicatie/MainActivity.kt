22389c9854f59f62348166bbeba0f85aac3d925b
Siebe Camerman <siebecamerman@gmail.com>
2023-11-21 16:16:43 +0100

    added android sdk

--- code/monitoringapplicatie/android/app/src/main/kotlin/com/example/monitoringapplicatie/MainActivity.kt
+++ code/monitoringapplicatie/android/app/src/main/kotlin/com/example/monitoringapplicatie/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.example.monitoringapplicatie
 
+import androidx.appcompat.app.AppCompatActivity
+import com.xsens.dot.android.sdk.DotSdk
+import com.xsens.dot.android.sdk.interfaces.DotDeviceCallback
+import com.xsens.dot.android.sdk.interfaces.DotScannerCallback
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+abstract class MainActivity : AppCompatActivity() , DotDeviceCallback, DotScannerCallback {
+    private fun initXsSdk() {
+        // Get the SDK version
+        val version: String = DotSdk.getSdkVersion()
+
+        // Enable debug mode
+        DotSdk.setDebugEnabled(true)
+
+        // Enable reconnect feature
+        DotSdk.setReconnectEnabled(true)
+    }
+
 }
+
+
