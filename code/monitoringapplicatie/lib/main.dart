0d3277699685bb35e2772b78bcc69867d3e69946
Seppe Stroobants <seppestroobants@Seppes-MBP.lan>
2023-11-24 15:21:25 +0100

    firestore test done

--- code/monitoringapplicatie/lib/main.dart
+++ code/monitoringapplicatie/lib/main.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:monitoringapplicatie/pages/account.dart';
 import 'package:flutter/services.dart';
+import 'package:monitoringapplicatie/pages/firestore_test.dart';
 
-void main() => runApp(MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Account(),
-      },
-    ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const firestore_test(),
+    },
+  ));
+}
