505777cae0b3b4661c443b8f67ce140a3d711a17
Siebe Camerman <siebecamerman@gmail.com>
2023-12-01 15:13:29 +0100

    ble scan

--- code/monitoringapplicatie/android/app/src/main/kotlin/com/example/monitoringapplicatie/MainActivity.kt
+++ code/monitoringapplicatie/android/app/src/main/kotlin/com/example/monitoringapplicatie/MainActivity.kt
@@ -24,7 +24,7 @@ import android.content.pm.PackageManager
 import android.Manifest
 import android.os.Build
 import androidx.core.app.ActivityCompat
-
+import com.xsens.dot.android.sdk.models.DotDevice
 
 
 class MainActivity: FlutterActivity(), DotScannerCallback{
@@ -35,6 +35,8 @@ class MainActivity: FlutterActivity(), DotScannerCallback{
   // A variable for scanning flag
   private var mIsScanning = false
 
+  // A list contains scanned Bluetooth device
+  private val mScannedSensorList = ArrayList<HashMap<String, Any>>()
 
   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
     super.configureFlutterEngine(flutterEngine)
@@ -42,37 +44,42 @@ class MainActivity: FlutterActivity(), DotScannerCallback{
         // This method is invoked on the main thread.
         call, result ->
         when (call.method) {
-             "getBatteryLevel" -> {
-              val batteryLevel = getBatteryLevel()
+          "getBatteryLevel" -> {
+          val batteryLevel = getBatteryLevel()
 
-              if (batteryLevel != -1) {
-              result.success(batteryLevel)
-              } else {
-              result.error("UNAVAILABLE", "Battery level not available.", null)
-              }}
-
-              "movella" -> {
-                  val status = initMovellaDotSdk()
-                  initXsScanner()
-                  //This permission is only required in Android S or higher
-                  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ActivityCompat.checkSelfPermission(
-                          this@MainActivity,
-                          Manifest.permission.BLUETOOTH_SCAN
-                      ) != PackageManager.PERMISSION_GRANTED
-                  ) {
-                      ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.BLUETOOTH_SCAN), 1000)
-                  } else {
-                      mIsScanning = if (mXsScanner == null) false else mXsScanner!!.startScan()
-                  }
-                  // mXsScanner!!.startScan()
-                  result.success("$status, $mIsScanning")
-              }
-              "movella_stop" -> {
-                  mIsScanning = !mXsScanner!!.stopScan()
-                  result.success("$mIsScanning")
-              }
+          if (batteryLevel != -1) {
+            result.success(batteryLevel)
+          } else {
+          result.error("UNAVAILABLE", "Battery level not available.", null)
+          }}
+          "movella_init" -> {
+            val status = initMovellaDotSdk()
+            initXsScanner()
+            result.success(status)
+          }
+          "movella_BLEscan" -> {
+            // val status = initMovellaDotSdk()
+            // initXsScanner()
+            //check if permissions are granted
+            if(checkPermissions()){
+              mIsScanning = if (mXsScanner == null) false else mXsScanner!!.startScan()
+            }
+            if(mIsScanning){
+              // val test = connectDevice(mScannedSensorList)
+              Log.i(TAG, "device: ")
+              result.success("$mIsScanning")
+            }
+            else{
+              result.success("Scanning? $mIsScanning")
+            }
+              
+          }
+          "movella_stop" -> {
+            mIsScanning = !mXsScanner!!.stopScan()
+            result.success("$mIsScanning, $mScannedSensorList")
+          }
         else -> {
-            result.notImplemented()
+          result.notImplemented()
         }
       } 
     }
@@ -91,62 +98,112 @@ class MainActivity: FlutterActivity(), DotScannerCallback{
   return batteryLevel
 }
 
-
-    
-
-    override fun onDotScanned(device: BluetoothDevice, rssi: Int) {
-      Log.i(TAG, "onXsensDotScanned() - Name: " + device.getName() + ", Address: " + device.getAddress());
-        // if (isAdded) {
-
-        //     // Use the mac address as UID to filter the same scan result.
-        //     var isExist = false
-        //     for (map in mScannedSensorList) {
-        //         if ((map[ScanAdapter.KEY_DEVICE] as BluetoothDevice?)!!.address == device.address) isExist = true
-        //     }
-        //     if (!isExist) {
-
-        //         // The original connection state is Disconnected.
-        //         // Also set tag, battery state, battery percentage to default value.
-        //         val map = HashMap<String, Any>()
-        //         map[ScanAdapter.KEY_DEVICE] = device
-        //         map[ScanAdapter.KEY_CONNECTION_STATE] = DotDevice.CONN_STATE_DISCONNECTED
-        //         map[ScanAdapter.KEY_TAG] = ""
-        //         map[ScanAdapter.KEY_BATTERY_STATE] = -1
-        //         map[ScanAdapter.KEY_BATTERY_PERCENTAGE] = -1
-        //         mScannedSensorList.add(map)
-        //         mScanAdapter!!.notifyItemInserted(mScannedSensorList.size - 1)
-        //     }
-        // }
-    }
-
-    /**
-     * Setup for Movella DOT SDK.
-     */
-    private fun initMovellaDotSdk(): String {
-
-        // Get the version name of SDK.
-        val version = DotSdk.getSdkVersion()
-        Log.i(TAG, "initMovellaDotSdk() - version: $version")
-
-        // Enable this feature to monitor logs from SDK.
-        DotSdk.setDebugEnabled(true)
-        // Enable this feature then SDK will start reconnection when the connection is lost.
-        DotSdk.setReconnectEnabled(true)
-
-        return version
-    }
-
-
-    private fun initXsScanner() {
-      // Check Bluetooth permissions at runtime
-        if (mXsScanner == null) {
-            mXsScanner = DotScanner(context, this)
-            mXsScanner!!.setScanMode(ScanSettings.SCAN_MODE_BALANCED)
-        }
-    }
-
-
-    companion object {
-        private val TAG = MainActivity::class.java.simpleName
-    }
+//hier is kanker
+//chatgpt sucked
+//terug opnieuw bekijken en zien dat ik <list<list<string>>> terug krijg
+private fun connectDevice(scannedSensorList: List<HashMap<String, Any>>): String {
+  return "kanker"
 }
+
+
+
+
+
+
+private fun checkPermissions(): Boolean {
+  //This permission is only required in Android S or higher
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S &&
+        (ActivityCompat.checkSelfPermission(
+            this@MainActivity,
+            Manifest.permission.BLUETOOTH_SCAN
+        ) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED)
+    ) {
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            1000
+        )
+    }
+    else{
+      return true
+    }
+    return false
+  }
+
+
+override fun onDotScanned(device: BluetoothDevice, rssi: Int) {
+    Log.i(TAG, "onDotScanned() - Name: ${device.name}, Address: ${device.address}")
+
+  // Your logic for handling the scanned device
+  // For example, add the device to a list or perform some other action
+
+  // Use the mac address as UID to filter the same scan result.
+  
+  var isExist = false
+  for (map in mScannedSensorList) {
+      if ((map["device"] as BluetoothDevice?)?.address == device.address) {
+          isExist = true
+          break
+      }
+  }
+
+  if (!isExist) {
+      // The original connection state is Disconnected.
+      // Also set tag, battery state, battery percentage to default value.
+      val map = HashMap<String, Any>()
+      map["device"] = device
+      map["connectionState"] = DotDevice.CONN_STATE_DISCONNECTED
+      map["tag"] = ""
+      map["batteryState"] = -1
+      map["batteryPercentage"] = -1
+
+      mScannedSensorList.add(map)
+      // Notify your application or perform the necessary UI update
+  }
+}
+
+
+  /**
+    * Setup for Movella DOT SDK.
+    */
+  private fun initMovellaDotSdk(): String {
+
+      // Get the version name of SDK.
+      val version = DotSdk.getSdkVersion()
+      Log.i(TAG, "initMovellaDotSdk() - version: $version")
+
+      // Enable this feature to monitor logs from SDK.
+      DotSdk.setDebugEnabled(true)
+      // Enable this feature then SDK will start reconnection when the connection is lost.
+      DotSdk.setReconnectEnabled(true)
+
+      return version
+  }
+
+
+  private fun initXsScanner() {
+    // Check Bluetooth permissions at runtime
+      if (mXsScanner == null) {
+          mXsScanner = DotScanner(context, this)
+          mXsScanner!!.setScanMode(ScanSettings.SCAN_MODE_BALANCED)
+      }
+  }
+
+
+  companion object {
+      private val TAG = MainActivity::class.java.simpleName
+  }
+}
+
