3293b209260a7333d9a08d516224ae1fbb2e928c
Syan Delbart <syan@syandelbart.com>
2023-12-11 00:02:28 +0100

    Set the wrapper as the main route, not sure if this is best practice

--- code/monitoringapplicatie/lib/main.dart
+++ code/monitoringapplicatie/lib/main.dart
@@ -3,16 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:monitoringapplicatie/pages/account.dart';
 import 'package:monitoringapplicatie/pages/demo.dart';
 import 'package:monitoringapplicatie/pages/firestore_test.dart';
+import 'package:monitoringapplicatie/pages/wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(MaterialApp(
-    initialRoute: '/demo',
-    routes: {
-      '/': (context) => const Account(),
-      '/demo': (context) => const Demo(),
-      '/firestore_test': (context) => const firestore_test(),
-    },
+    initialRoute: '/',
+    routes: {'/': (context) => const Wrapper()},
   ));
 }
