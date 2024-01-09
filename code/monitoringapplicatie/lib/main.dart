c603e29207f0154ef562d0c32a159f72640bd6db
Syan Delbart <syan@syandelbart.com>
2024-01-09 16:20:52 +0100

    Add Firebase authentication and options

--- code/monitoringapplicatie/lib/main.dart
+++ code/monitoringapplicatie/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:monitoringapplicatie/pages/account.dart';
 import 'package:monitoringapplicatie/pages/demo.dart';
@@ -6,9 +8,11 @@ import 'package:monitoringapplicatie/pages/demo_real.dart';
 import 'package:monitoringapplicatie/pages/firestore_test.dart';
 import 'package:monitoringapplicatie/pages/Navbar.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(MaterialApp(
     initialRoute: '/demo_real',
@@ -17,6 +21,7 @@ void main() async {
       '/demo': (context) => const NavBar(child: Demo()),
       '/demo_real': (context) => const NavBar(child: DemoReal()),
       '/firestore_test': (context) => const NavBar(child: firestore_test()),
+      '/account': (context) => const NavBar(child: Account()),
     },
   ));
 }
