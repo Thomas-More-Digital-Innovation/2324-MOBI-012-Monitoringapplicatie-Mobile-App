0e5b39979c867e494a222f22f832801e54c0a75a
Siebe Camerman <siebecamerman@gmail.com>
2024-02-14 13:00:43 +0100

    test geschreven

--- code/monitoringapplicatie/test/demo_sdk_page_test.dart
+++ code/monitoringapplicatie/test/demo_sdk_page_test.dart
@@ -1,25 +1,18 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:monitoringapplicatie/pages/demo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:monitoringapplicatie/pages/account.dart';
 
 void main() {
-  testWidgets('Test initial SDK testing page values', (tester) async {
-    // Create the widget by telling the tester to build it.
-
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: MyHomePage(title: 'Title'),
-        ),
+  testWidgets('finds a Text widget', (tester) async {
+    // Build an App with a Text widget that displays the letter 'H'.
+    await tester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: Text('H'),
       ),
-    );
+    ));
 
-    final textFinder = find.text('Unknown battery level.');
-    final measureMentTextFinder = find.text('Unknown');
-
-    expect(textFinder, findsOneWidget);
-    expect(measureMentTextFinder, findsExactly(2));
-    // final sdkTextFinder = find.text('2023 1.0');
-    // expect(sdkTextFinder, findsOneWidget);
+    // Find a widget that displays the letter 'H'.
+    expect(find.text('H'), findsOneWidget);
   });
 }
